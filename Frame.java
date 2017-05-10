b39873e20c6a45be2a9411f85464b78124935003
bowinhampe <hampusbowin@gmail.com>
2017-05-10 09:53:08 +0200

    Implementation for User story 1
    Created frame class with constructor taking first and second value then setting the private values to these;
    Added two get functions to retrieve the values;

---
+++ Frame.java
@@ -0,0 +1,17 @@
+
+public class Frame {
+	private int firstTurn;
+	private int secondTurn;
+	
+	public Frame(int first, int second){
+		firstTurn = first;
+		secondTurn = second;
+	}
+	
+	public int getFirstTurn(){
+		return firstTurn;
+	}
+	public int getSecondTurn(){
+		return secondTurn;
+	}
+}
