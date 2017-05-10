37c603b2c48cb5b85625103d966033b75fba6a90
bowinhampe <hampusbowin@gmail.com>
2017-05-10 10:00:09 +0200

    Test case for User story 3
    The tests creates a game and adds 10 frames, then calls function to get the number of frames to compare with 10.

--- TestCases.java
+++ TestCases.java
@@ -24,5 +24,23 @@ public class TestCases {
 		assertEquals(expectedValue, frame.computeScore());
 
 	}
+	
+	@Test
+	public void testGame() {
+		Game game = new Game();
+		game.addFrame(1,5);
+		game.addFrame(3,6);
+		game.addFrame(7,2);
+		game.addFrame(3,6);
+		game.addFrame(4,4);
+		game.addFrame(5,3);
+		game.addFrame(3,3);
+		game.addFrame(4,5);
+		game.addFrame(8,1);
+		game.addFrame(2,6);
+		int actual = game.getNrOfFrames();
+		int expected = 10;
+		assertEquals(expected, actual);
+	}
 
 }
