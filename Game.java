d9a1da7ae80cfb9655e63dcd57c0c845ec539b31
bowinhampe <hampusbowin@gmail.com>
2017-05-10 14:10:14 +0200

    Implementation for User story 10
    Added ArrayList containing bonusFrames;
    Added function "addBonusFrame()";
    Added if statement to check if last frame is a spare;
    
    This implementation is simple because it has clear instructions in code on what it does and it is only a few lines.

--- Game.java
+++ Game.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 
 public class Game {
 	private ArrayList<Frame> gameFrames;
+	private ArrayList<Frame> bonusFrames;
 	
 	public Game(){
 		gameFrames = new ArrayList<>();
+		bonusFrames = new ArrayList<>();
 	}
 	public void addFrame(int firstValue, int secondValue){
 		Frame frame = new Frame(firstValue, secondValue);
 		gameFrames.add(frame);
+	}
+	public void addBonusFrame(int bonusValue){
+		Frame frame = new Frame(bonusValue,0);
+		bonusFrames.add(frame);
 	}
 	public int getNrOfFrames(){
 		return gameFrames.size();
@@ -18,6 +24,7 @@ public class Game {
 		for(int i = 0; i < gameFrames.size();i++){
 			// Check if it is a strike
 			if(gameFrames.get(i).isStrike()){
+				// Check if there is more strikes
 				if(gameFrames.get(i+1).isStrike()){
 					gameScore += gameFrames.get(i).computeScore() + gameFrames.get(i+1).computeScore() + gameFrames.get(i+2).getFirstTurn();
 				}
@@ -27,7 +34,13 @@ public class Game {
 			}
 			// Check if it is a spare
 			else if(gameFrames.get(i).computeScore()==10){
+				// Check if it is the last frame
+				if(i==gameFrames.size()-1){
+					gameScore += gameFrames.get(i).computeScore() + bonusFrames.get(0).getFirstTurn();
+				}
+				else{
 				gameScore += gameFrames.get(i).computeScore() + gameFrames.get(i+1).getFirstTurn();
+				}
 			}
 			else{
 				gameScore += gameFrames.get(i).computeScore();
